89ae49d092e4c36a99e115e8c04f133704acd6cd
Roman Malinovski <roman.malinovski@gmail.com>
2025-02-18 11:52:01 +0300

    CreateProductCommandHandler initial implementation

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -1,12 +1,22 @@
 ﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
 
 namespace CatalogAPI.Products.CreateProduct
 {
     internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            Product product = new Product
+            {
+                Name = command.Name,
+                Categories = command.Categories,
+                Description = command.Descrption,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
