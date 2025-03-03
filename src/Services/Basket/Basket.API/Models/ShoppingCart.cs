1d2a7ec98b087c9cd0eedc250d97c7ab557f5444
Roman Malinovski <roman.malinovski@gmail.com>
2025-03-03 15:49:10 +0300

    added Basket domain models

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+        
+        public ShoppingCart()        {
+            
+        }
+
+        public string UserName { get; set; } = default!;
+
+        public IList<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+    }
+}
