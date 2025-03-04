e59b4b94287c00920294f43fed3a69866fac6e40
Roman Malinovski <roman.malinovski@gmail.com>
2025-03-04 12:04:38 +0300

    delete basket endpoint

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,6 +1,29 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+namespace Basket.API.Basket.DeleteBasket
 {
-    public class DeleteBasketEndpoint
+    public record DeleteBasketRequest(string UserName);
+    public record DeleteBasketResponse(bool IsSuccess);
+
+    public class DeleteBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket/{userName}", async (DeleteBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<DeleteBasketCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<DeleteBasketResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("DeleteBasket")
+                .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Delete Basket")
+                .WithDescription("Delete Basket");
+        }
     }
 }
