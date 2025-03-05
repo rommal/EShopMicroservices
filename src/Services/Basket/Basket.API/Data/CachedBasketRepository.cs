3e47fbeb629fa3d26cbf1bcaf3a5069ab16d9c6e
Roman Malinovski <roman.malinovski@gmail.com>
2025-03-05 16:08:27 +0300

    added CachedBasketRepository

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache) : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
+
+            ShoppingCart? basket = null;
+
+            if (!string.IsNullOrEmpty(cachedBasket))
+            {
+                basket = JsonSerializer.Deserialize<ShoppingCart>(cachedBasket);
+            }
+            
+            if (basket == null)
+            {
+                basket = await repository.GetBasket(userName, cancellationToken);
+                await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+            }                
+
+            return basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            var result = await repository.StoreBasket(basket, cancellationToken);
+
+            await cache.SetStringAsync(result.UserName, JsonSerializer.Serialize(result), cancellationToken);
+
+            return result;
+        }
+
+        public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var result = await repository.DeleteBasket(userName, cancellationToken);
+
+            await cache.RemoveAsync(userName, cancellationToken);
+
+            return result;
+        }        
+    }
+}
