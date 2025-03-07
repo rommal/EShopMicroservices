b0426a77755fa0ea4020fbdc876abdd6f8439d43
Roman Malinovski <roman.malinovski@gmail.com>
2025-03-07 11:54:58 +0300

    added DiscountService to DI

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,6 +1,27 @@
-﻿namespace Discount.Grpc.Services
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services
 {
-    public class DiscountService
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
     {
+        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            return base.GetDiscount(request, context);
+        }
+
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }        
     }
 }
