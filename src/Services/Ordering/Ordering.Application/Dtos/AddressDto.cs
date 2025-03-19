a0356f41e5a73e008577cd814dbb2696c5d66716
Roman Malinovski <roman.malinovski@gmail.com>
2025-03-19 15:23:10 +0300

    added ordering dtos

---
+++ src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -0,0 +1,3 @@
+﻿namespace Ordering.Application.Dtos;
+
+public record AddressDto(string FirstName, string LastName, string EmailAdress, string AddressLine, string Country, string State, string ZipCode);
