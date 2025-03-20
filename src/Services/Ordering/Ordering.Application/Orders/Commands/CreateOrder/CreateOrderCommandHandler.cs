6979f5c933d524fedc50843f508ca0784a1a886b
Roman Malinovski <roman.malinovski@gmail.com>
2025-03-20 14:50:35 +0300

    implemented create order handler

--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -1,12 +1,40 @@
-﻿using BuildingBlocks.CQRS;
-
-namespace Ordering.Application.Orders.Commands.CreateOrder
+﻿namespace Ordering.Application.Orders.Commands.CreateOrder
 {
-    public class CreateOrderCommandHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    public class CreateOrderCommandHandler(IApplicationDbContext dbContext) : ICommandHandler<CreateOrderCommand, CreateOrderResult>
     {
-        public async Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
         {
+            var order = CreateOrder(command.Order);
+
+            dbContext.Orders.Add(order);
+            await dbContext.SaveChangesAsync(cancellationToken);
+
             throw new NotImplementedException();
         }
-    }
+
+        private Order CreateOrder(OrderDto orderDto) 
+        {
+            var shippingAddress = Address.Of(orderDto.ShippingAddress.FirstName, orderDto.ShippingAddress.LastName, orderDto.ShippingAddress.EmailAdress, 
+                orderDto.ShippingAddress.AddressLine, orderDto.ShippingAddress.Country, orderDto.ShippingAddress.State, orderDto.ShippingAddress.ZipCode);
+            var billingAddress = Address.Of(orderDto.BillingAddress.FirstName, orderDto.BillingAddress.LastName, orderDto.BillingAddress.EmailAdress, 
+                orderDto.BillingAddress.AddressLine, orderDto.BillingAddress.Country, orderDto.BillingAddress.State, orderDto.BillingAddress.ZipCode);
+
+            var newOrder = Order.Create(
+                OrderId.Of(Guid.NewGuid()), 
+                CustomerId.Of(orderDto.CustomerId), 
+                OrderName.Of(orderDto.OrderName),
+                shippingAddress, 
+                billingAddress, 
+                Payment.Of(orderDto.Payment.CardName, orderDto.Payment.CardNumber, orderDto.Payment.Expiration, orderDto.Payment.Cvv, orderDto.Payment.PaymentMethod));
+
+            foreach (var orderItemDto in orderDto.OrderItems)
+            {
+                newOrder.Add(ProductId.Of(orderItemDto.ProductId), orderItemDto.Quantity, orderItemDto.Price);
+            }
+
+            return newOrder;
+        }
+    }   
 }
+
+
