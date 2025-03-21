b731d93638720364a9cace692567b1b1e6870e05
Roman Malinovski <roman.malinovski@gmail.com>
2025-03-21 17:46:12 +0300

    get orders endpoint implementaion

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,6 +1,29 @@
-﻿namespace Ordering.API.Endpoints
+﻿
+using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints
 {
-    public class GetOrders
+    //public record GetOrdersRequest(int Page, int );
+    public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
+
+    public class GetOrders : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders", async ([AsParameters] PagenationRequest request, ISender sender) =>
+            {
+                var result = await sender.Send(new GetOrdersQuery(request));
+
+                var response = result.Adapt<GetOrdersResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("GetOrders")
+                .Produces<GetOrdersByNameResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)                
+                .WithSummary("Get Orders")
+                .WithDescription("Get Orders");
+        }
     }
 }
