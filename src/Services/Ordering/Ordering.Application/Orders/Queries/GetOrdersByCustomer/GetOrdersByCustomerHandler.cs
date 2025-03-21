e562c9a44440e37f4c77bdb0d46e3e0a4581c633
Roman Malinovski <roman.malinovski@gmail.com>
2025-03-21 11:55:28 +0300

    added get orders by customer feature

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public class GetOrdersByCustomerHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+    {
+        public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await dbContext.Orders
+                .Include(e => e.OrderItems)
+                .AsNoTracking()
+                .Where(e => e.CustomerId == CustomerId.Of(query.CustomerId))
+                .OrderBy(e => e.OrderName.Value)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+        }
+    }
+}
