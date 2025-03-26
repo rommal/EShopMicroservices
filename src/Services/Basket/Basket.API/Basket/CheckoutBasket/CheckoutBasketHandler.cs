8e92882766c75b9c9729746ed0204373a4d7208f
Roman Malinovski <roman.malinovski@gmail.com>
2025-03-26 15:52:27 +0300

    implemented checkout basket  handler

--- src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
@@ -1,14 +1,31 @@
 ﻿
+using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
 namespace Basket.API.Basket.CheckoutBasket
 {
     public record CheckoutBasketCommand(BasketCheckoutDto BasketCheckout) : ICommand<CheckoutBasketResult>;
     public record CheckoutBasketResult(bool IsSuccess);
 
-    public class CheckoutBasketHandler : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+    public class CheckoutBasketHandler(IBasketRepository repository, IPublishEndpoint publishEndpoint)
+        : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
     {
-        public Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+        public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var basket = await repository.GetBasket(command.BasketCheckout.UserName, cancellationToken);
+            if (basket == null) 
+            {
+                return new CheckoutBasketResult(false);
+            }
+
+            var eventmessage = command.BasketCheckout.Adapt<BasketCheckoutEvent>();
+            eventmessage.TotalPrice = basket.TotalPrice;
+
+            await publishEndpoint.Publish(eventmessage, cancellationToken);
+
+            await repository.DeleteBasket(command.BasketCheckout.UserName, cancellationToken);
+
+            return new CheckoutBasketResult(true);
         }
     }
 }
---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Basket.CheckoutBasket
+{
+    public class CheckoutBasketValidator: AbstractValidator<CheckoutBasketCommand>
+    {
+        public CheckoutBasketValidator() 
+        {
+            RuleFor(x => x.BasketCheckout).NotNull().WithMessage("BasketCheckout can't be null.");
+            RuleFor(x => x.BasketCheckout.UserName).NotEmpty().WithMessage("UserName is required.");
+        }
+    }
+}
