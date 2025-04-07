fc40a8a375dddf65de875f6d75aaba03b3f33ae1
Roman Malinovski <roman.malinovski@gmail.com>
2025-04-07 18:09:51 +0300

    added product model to shopping web

---
+++ src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Shopping.Web.Models.Catalog
+{
+    public class ProductModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public IList<string> Categories { get; set; } = new List<string>();
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+
+    public record GetProductsResponse(IEnumerable<ProductModel> Products);
+    public record GetProductsByCategoryResponse(IEnumerable<ProductModel> Products);
+    public record GetProductsByIdResponse(ProductModel Product);
+}
