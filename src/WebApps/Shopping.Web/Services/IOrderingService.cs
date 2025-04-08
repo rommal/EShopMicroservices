5ed94ea9fa23ab117329b7618743443dc2fcd000
Roman Malinovski <roman.malinovski@gmail.com>
2025-04-08 16:18:20 +0300

    added IOrderingServive to shopping web app

---
+++ src/WebApps/Shopping.Web/Services/IOrderingService.cs
@@ -0,0 +1,16 @@
+﻿using Refit;
+
+namespace Shopping.Web.Services
+{
+    public interface IOrderingService
+    {
+        [Get("/ordering-service/orders?pageIndex={pageIndex}&pageSize={pageSize}\"")]
+        Task<GetOrdersResponse> GetOrders(int? PageNumber = 1, int? PageSize = 10);
+
+        [Get("/ordering-service/orders/{orderName}")]
+        Task<GetOrdersByNameResponse> GetOrdersByName(string orderName);
+
+        [Get("/ordering-service/orders/customer/{customerId}")]
+        Task<GetOrdersByCustomerResponse> GetOrdersByCustomer(Guid customerId);
+    }
+}
